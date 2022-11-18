d217e015a1c2bddb1ecd87b976b39db7cc9dc5b7
TolhurstBiometrics <59974876+TolhurstBiometrics@users.noreply.github.com>
2022-11-18 17:57:25 +0000

    updating ext var part

--- R/field_trial_error.R
+++ R/field_trial_error.R
@@ -1,13 +1,14 @@
+
 #' Simulate plot errors for a plant breeding trial
 #'
 #' Creates a data frame with simulated plot errors for one or more traits in plant breeding
 #' trials across multiple environments. The simulated error consists of a spatial error term,
 #' a random error term and an extraneous error term. The spatial error term is constructed
-#' according to either 1) bivariate interpolation using the \link[interp]{interp} function of 
-#' the package 'interp', or 2) a separable first-order autoregressive process (AR1:AR1). The 
-#' random error term is constructed using an independent process. The extraneous error term is 
+#' according to either 1) bivariate interpolation using the \link[interp]{interp} function of
+#' the package 'interp', or 2) a separable first-order autoregressive process (AR1:AR1). The
+#' random error term is constructed using an independent process. The extraneous error term is
 #' constructed as the sum of column and/or row terms. something more here?
-#' The spatial, random and extraneous error terms are combined according to a user-defined ratio. 
+#' The spatial, random and extraneous error terms are combined according to a user-defined ratio.
 #' \cr
 #' For multiple traits, correlated error terms can be generated assuming 1) correlated spatial
 #' error between traits, 2) correlated random error between traits, 3) correlated extraneous
@@ -43,11 +44,11 @@
 #' @param R_cor_R A matrix of random error correlations between more than one trait. If not
 #'   defined and \code{n_traits > 1}, a diagonal matrix is constructed.
 #' @param E_cor_R A matrix of extraneous error correlations between more than one trait. If not
-#'   defined and \code{n_traits > 1}, a diagonal matrix is constructed. The same correlation is 
+#'   defined and \code{n_traits > 1}, a diagonal matrix is constructed. The same correlation is
 #'   assigned to the column and row errors.
-#' @param spatial_model A character string specifying the model used to simulate the spatial error
-#'   term. One of either "Bivariate" (bivariate interpolation, the default) or "AR1:AR1"
-#'   (separable first-order autoregressive process (AR1:AR1)).
+#' @param spatial_model A character string specifying the model used to simulate the two-dimensional
+#'   spatial error term. One of either "Bivariate" (bivariate interpolation, the default) or "AR1:AR1"
+#'   (separable first-order autoregressive process).
 #' @param complexity A scalar defining the complexity of the bivariate interpolation model.
 #'   By default, \code{complexity = 12}. Note that low values may lead to convergence problems.
 #'   See \link[interp]{interp} for further details.
@@ -58,15 +59,17 @@
 #'   spatial error model. If only one value is provided, all environments will be assigned the
 #'   same row autocorrelation.
 #' @param prop_spatial A vector defining the proportion of spatial error variance to total
-#'   error variance (spatial + random + extraneous) for each environment. If only one value is provided, 
-#'   all environments will be assigned the same proportion. By default, the spatial error variance 
+#'   error variance (spatial + random + extraneous) for each environment. If only one value is provided,
+#'   all environments will be assigned the same proportion. By default, the spatial error variance
 #'   accounts for half of the total error variance (\code{prop_spatial = 0.5}).
 #' @param prop_Ext A vector defining the proportion of extraneous error variance to total
-#'   error variance (spatial + random + extraneous) for each environment. If only one value is provided, 
-#'   all environments will be assigned the same proportion. By default, the extraneous error variance 
+#'   error variance (spatial + random + extraneous) for each environment. If only one value is provided,
+#'   all environments will be assigned the same proportion. By default, the extraneous error variance
 #'   is zero (\code{prop_ext = 0}).
+#' @param ext_var A character string specifying the direction of extraneous variation. One of either
+#'   "column", "row" or "both".
 #' @param return_effects When TRUE, a list is returned with additional entries for each trait
-#'   containing the spatial and random errors. By default, return_effects = FALSE.
+#'   containing the spatial and random errors. By default, return_effects = FALSE. # <-- here
 #'
 #' @return A data frame containing the environment id, block id, column id, row id, and the
 #'   simulated error for each trait. When \code{return_effects = TRUE}, a list is returned with
@@ -108,10 +111,10 @@
 #'   rep_dir = "row",
 #'   var_R = var_R,
 #'   S_cor_R = S_cor_R,
-#'   spatial_model = "bivariate",
+#'   spatial_model = "Bivariate",
 #'   complexity = 14,
 #'   prop_spatial = 0.6,
-#'   prop_ext = 0,
+#'   prop_ext = 0.2,
 #'   return_effects = TRUE
 #' )
 #' @export
@@ -133,6 +136,7 @@ field_trial_error <- function(n_envs,
                               row_cor = NULL,
                               prop_spatial = 0.5,
                               prop_ext = 0,
+                              ext_var = NULL,
                               return_effects = FALSE) {
   if (n_envs < 1 | n_envs %% 1 != 0) stop("'n_envs' must be an integer > 0")
   if (n_traits < 1 | n_traits %% 1 != 0) stop("'n_traits' must be an integer > 0")
@@ -199,10 +203,11 @@ field_trial_error <- function(n_envs,
 
   if (is.null(S_cor_R)) S_cor_R <- diag(n_traits)
   if (is.null(R_cor_R)) R_cor_R <- diag(n_traits)
+  if (is.null(E_cor_R)) E_cor_R <- diag(n_traits)
 
   spatial_model <- tolower(spatial_model)
   if (spatial_model != "bivariate" & spatial_model != "ar1:ar1") {
-    stop("'spatial_model' must be 'bivariate' or 'AR1:AR1'")
+    stop("'spatial_model' must be 'Bivariate' or 'AR1:AR1'")
   }
 
   if (any(prop_spatial < 0) | any(prop_spatial > 1)) {
@@ -219,10 +224,10 @@ field_trial_error <- function(n_envs,
   if (length(prop_ext) != n_envs) {
     stop("Length of vector 'prop_ext' does not match total number of environments")
   }
-    if (any(prop_spatial + prop_ext > 1)) {
+  if (any(prop_spatial + prop_ext > 1)) {
     stop("The sum of 'prop_spatial' and 'prop_ext' must be between 0 and 1")
   }
-      if (any(prop_spatial + prop_ext == 1)) {
+  if (any(prop_spatial + prop_ext == 1)) {
     warning("The random error is zero for some environments")
   }
   envs <- rep(1:n_envs, times = n_cols * n_rows)
@@ -270,7 +275,7 @@ field_trial_error <- function(n_envs,
 
     l_lst <- lapply(cor_mat_lst, function(x) chol(x))
     plot_error_lst1 <- mapply(function(x, y) matrix(c(stats::rnorm(x) %*% y), ncol = n_traits),
-      x = n_cols * n_rows * n_traits, y = l_lst, SIMPLIFY = FALSE
+                              x = n_cols * n_rows * n_traits, y = l_lst, SIMPLIFY = FALSE
     )
   }
 
@@ -279,12 +284,12 @@ field_trial_error <- function(n_envs,
 
     cols_lst <- with(plot_df, tapply(col, env, function(x) c(unique(x), max(x) + 1)))
     col_centres_lst <- mapply(function(x, y) round(y * (x - 0.5), 8),
-      x = cols_lst, y = plot_length, SIMPLIFY = FALSE
+                              x = cols_lst, y = plot_length, SIMPLIFY = FALSE
     )
 
     rows_lst <- with(plot_df, tapply(row, env, function(x) c(unique(x), max(x) + 1)))
     row_centres_lst <- mapply(function(x, y) round(y * (x - 0.5), 8),
-      x = rows_lst, y = plot_width, SIMPLIFY = FALSE
+                              x = rows_lst, y = plot_width, SIMPLIFY = FALSE
     )
 
     col_gap <- plot_length / 4
@@ -293,10 +298,10 @@ field_trial_error <- function(n_envs,
     plot_error_lst1 <- NA
     while (sum(is.na(unlist(plot_error_lst1))) > 0) {
       xInterp_list <- mapply(function(x, y, z) c(0 - z, x * y + z, 0 - z, x * y + z, sample(stats::runif(n = complexity, min = 0, max = (x * y)))),
-        x = n_cols, y = plot_length, z = col_gap, SIMPLIFY = FALSE
+                             x = n_cols, y = plot_length, z = col_gap, SIMPLIFY = FALSE
       )
       yInterp_list <- mapply(function(x, y, z) c(0 - z, 0 - z, x * y + z, x * y + z, sample(stats::runif(n = complexity, min = 0, max = (x * y)))),
-        x = n_rows, y = plot_width, z = row_gap, SIMPLIFY = FALSE
+                             x = n_rows, y = plot_width, z = row_gap, SIMPLIFY = FALSE
       )
       zInterp_list <- lapply(n_cols, function(x) scale(matrix(stats::rnorm((4 + complexity) * n_traits), ncol = n_traits)) %*% chol(S_cor_R))
 
@@ -317,31 +322,54 @@ field_trial_error <- function(n_envs,
     }
   }
 
-  plot_error_lst2 <- mapply(function(x) matrix(c(stats::rnorm(x)), ncol = n_traits) %*% chol(R_cor_R),
-    x = n_cols * n_rows * n_traits, SIMPLIFY = FALSE
+  plot_error_lst2 <- mapply(function(x) scale(matrix(c(stats::rnorm(x)), ncol = n_traits)),
+                            x = n_cols * n_rows * n_traits, SIMPLIFY = FALSE
   )
-                             
-  plot_error_lst3 <- mapply(function(x) matrix(c(stats::rnorm(x)), ncol = n_traits) %*% chol(E_cor_R),
-    x = n_cols * n_rows * n_traits, SIMPLIFY = FALSE
-  ) # working here
-                             
-  var_R <- as.data.frame(t(matrix(var_R, ncol = n_traits, byrow = TRUE)))
+  plot_error_lst2 <- mapply(function(x) scale(x %*% solve(chol(var(x))) %*% chol(R_cor_R)),
+                            x = plot_error_lst2, SIMPLIFY = FALSE
+  )
+  plot_error_lst3c <- mapply(function(x) scale(matrix(c(stats::rnorm(x)), ncol = n_traits)),
+                            x = n_cols * n_traits, SIMPLIFY = FALSE
+  )
+  plot_error_lst3c <- mapply(function(x) scale(x %*% solve(chol(var(x))) %*% chol(E_cor_R)),
+                            x = plot_error_lst3c, SIMPLIFY = FALSE
+  )
+  plot_error_lst3r <- mapply(function(x) scale(matrix(c(stats::rnorm(x)), ncol = n_traits)),
+                             x = n_rows * n_traits, SIMPLIFY = FALSE
+  )
+  plot_error_lst3r <- mapply(function(x) scale(x %*% solve(chol(var(x))) %*% chol(E_cor_R)),
+                           x = plot_error_lst3r, SIMPLIFY = FALSE
+  )
 
+  Zc <- Zr <- list()
+  plot_df$col <- factor(as.numeric(trimws(plot_df$col)))
+  for(i in 1:n_envs){Zc[[i]] <- model.matrix( ~ col-1, droplevels(plot_df[plot_df$env == i,]))}
+  plot_df$row <- factor(as.numeric(trimws(plot_df$row)))
+  for(i in 1:n_envs){Zr[[i]] <- model.matrix( ~ row-1, droplevels(plot_df[plot_df$env == i,]))}
+  if(ext_var == "column"){plot_error_lst3 <- mapply(function(w,x) w %*% x, w = Zc, x = plot_error_lst3c, SIMPLIFY = F)}
+  if(ext_var == "row"){plot_error_lst3 <- mapply(function(w,x) w %*% x, w = Zr, x = plot_error_lst3r, SIMPLIFY = F)}
+  if(ext_var == "both"){plot_error_lst3 <- mapply(function(w,x,y,z) w %*% x + y %*% z, w = Zc, x = plot_error_lst3c, y = Zr, z = plot_error_lst3r, SIMPLIFY = F)}
+
+  var_R <- as.data.frame(t(matrix(var_R, ncol = n_traits))) # changed to FALSE here
   var_R <- lapply(X = var_R, FUN = c)
   e_spat <- mapply(function(x, y) (scale(x) * sqrt(y)), x = plot_error_lst1, y = prop_spatial, SIMPLIFY = F)
   e_rand <- mapply(function(x, y, z) (scale(x) * sqrt(1 - y - z)), x = plot_error_lst2, y = prop_spatial, z = prop_ext, SIMPLIFY = F)
-  e_ext <- mapply(function(x, y) (scale(x) * sqrt(y)), x = plot_error_lst3, x = prop_ext, SIMPLIFY = F) # working
-                  
-  if (n_traits > 1) {
-    e_scale <- mapply(function(x, y) sqrt(diag(1 / diag(as.matrix(stats::var(x + y))))), x = e_spat, y = e_rand, SIMPLIFY = F)
+  e_ext <- mapply(function(x, y) (scale(x) * sqrt(y)), x = plot_error_lst3, y = prop_ext, SIMPLIFY = F)
+
+if (n_traits > 1) {
+    e_scale <- mapply(function(x, y, z) sqrt(diag(1 / diag(as.matrix(stats::var(x + y + z))))), x = e_spat, y = e_rand, z = e_ext, SIMPLIFY = F)
     e_spat <- mapply(function(x, y, z) x %*% y %*% diag(sqrt(z)), x = e_spat, y = e_scale, z = var_R, SIMPLIFY = F)
     e_rand <- mapply(function(x, y, z) x %*% y %*% diag(sqrt(z)), x = e_rand, y = e_scale, z = var_R, SIMPLIFY = F)
+    e_ext <- mapply(function(x, y, z) x %*% y %*% diag(sqrt(z)), x = e_ext, y = e_scale, z = var_R, SIMPLIFY = F)
   }
-  if (n_traits == 1) {
+
+if (n_traits == 1) {
     e_scale <- mapply(function(x, y) sqrt(1 / stats::var(x + y)), x = e_spat, y = e_rand, SIMPLIFY = F)
     e_spat <- mapply(function(x, y, z) x %*% y %*% sqrt(z), x = e_spat, y = e_scale, z = var_R, SIMPLIFY = F)
     e_rand <- mapply(function(x, y, z) x %*% y %*% sqrt(z), x = e_rand, y = e_scale, z = var_R, SIMPLIFY = F)
-  }
+    e_ext <- mapply(function(x, y, z) x %*% y %*% sqrt(z), x = e_ext, y = e_scale, z = var_R, SIMPLIFY = F)
+}
+
   plot_error_lst <- mapply(function(x, y) x + y, x = e_spat, y = e_rand, SIMPLIFY = F)
   plot_error <- do.call(what = "rbind", plot_error_lst)
   colnames(plot_error) <- paste0("e.Trait.", 1:n_traits)
@@ -353,8 +381,8 @@ field_trial_error <- function(n_envs,
     e_all <- lapply(seq_len(ncol(e_spat)), function(i) cbind(e_spat[, i], e_rand[, i]))
     resids <- lapply(e_all, function(x) {
       data.frame(plot_df[, 1:4],
-        e_spatial = x[, 1],
-        e_random = x[, 2]
+                 e_spatial = x[, 1],
+                 e_random = x[, 2]
       )
     })
 
@@ -366,3 +394,5 @@ field_trial_error <- function(n_envs,
   plot_df
   return(plot_df)
 }
+
+
