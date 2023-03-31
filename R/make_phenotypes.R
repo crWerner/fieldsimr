c40a241461dc756dfa3fa31b8072d80be6c77c27
TolhurstBiometrics <59974876+TolhurstBiometrics@users.noreply.github.com>
2023-03-31 14:13:55 -0500

    word fixes

--- R/make_phenotypes.R
+++ R/make_phenotypes.R
@@ -1,23 +1,23 @@
-#' Phenotype simulation through combination of genetic values and plot errors
+#' Simulation of phenotypes - combining simulated genetic values and plot errors
 #'
-#' Creates a data frame of simulated field trial phenotypes for one or more traits through the
-#' combination of simulated plot errors with genetic values (e.g. true, simulated or predicted).
-#' If the genetic values were obtained externally, they have to be arranged in a data frame with
-#' columns "env", "rep", and "id", followed by the genetic values for each trait.
+#' Creates a data frame of simulated phenotypes for one or more traits by
+#' combining simulated plot errors with genetic values (e.g. true, simulated or predicted).
+#' The genetic values can be generated externally, but note that they must be stored in a
+#' data frame as described below.
 #'
-#' @param gv_df A data frame of genetic values. Must contain the columns "env", "rep", and "id",
+#' @param gv_df A data frame of genetic values. Must contain the columns `env', `rep', and `id',
 #'   followed by the genetic values for each trait.
-#' @param error_df A data frame of plot errors. Must contain the columns "env", "block",
-#'   "col", and "row", followed by the error values for each trait.
-#' @param randomise When TRUE, genotypes are randomly allocated to plots within blocks to simulate
+#' @param error_df A data frame of plot errors. Must contain the columns `env', `block',
+#'   `col', and `row', followed by the plot errors for each trait.
+#' @param randomise When TRUE, genotypes are randomly allocated to plots within blocks to generate
 #'   a randomized complete block design (RCBD).\cr
-#'   \strong{Note:} other experimental designs must be generated externally.
+#'   \strong{Note:} Other experimental designs must be generated externally.
 #'
-#' @return A data-frame containing the environment id, block id, column id, row id, genotype id,
-#'   and phenotypic values for each trait.
+#' @return A data frame with columns `env', `block', `column', `row' and `genotype', followed by
+#'   the phenotypes for each trait.
 #'
 #' @examples
-#' # Create phenotypes through combination of the the genetic values and plot errors provided in
+#' # Simulate phenotypes by combining the genetic values and plot errors provided in
 #' # the two example data frames 'df_gv_unstr' and 'df_error_bivar'.
 #'
 #' gv_df <- df_gv_unstr
