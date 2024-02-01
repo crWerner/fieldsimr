89c4f5fe6829b778659fd6bcb5a6a7abe3bf3561
TolhurstBiometrics <59974876+TolhurstBiometrics@users.noreply.github.com>
2024-02-02 10:55:34 +1100

    Update compound_symmetry_gxe.R
    replaced gxe_env with gxe_int
    error fixed that occurred when ntraits = 1
    printing Env number when return_effects = T (previously pseudo trait was printed)

--- R/compound_symmetry_gxe.R
+++ R/compound_symmetry_gxe.R
@@ -450,30 +450,31 @@ compsym_asr_output <- function(pop,
   ids <- factor(as.numeric(as.character(pop@id)))
 
   g_main <- as.list(as.data.frame(pop@gv[, seq(1, (n_traits + n_traits * n_envs), (n_envs + 1))]))
-  gxe_env <- pop@gv[, -seq(1, (n_traits + n_traits * n_envs), n_envs + 1)]
+  gxe_int <- pop@gv[, -seq(1, (n_traits + n_traits * n_envs), n_envs + 1)]
   index <- as.list(as.data.frame(t(matrix(1:(n_traits * n_envs), ncol = n_traits))))
-  gxe_env <- lapply(index, function(x) gxe_env[, x])
+  gxe_int <- lapply(index, function(x) gxe_int[, x])
 
-  gv <- lapply(gxe_env, function(x) x + do.call(cbind, g_main))
-  gv <- do.call(rbind, mapply(function(x, y) x[rep(1:nrow(x), y), ], x = gv, y = as.list(n_reps), SIMPLIFY = F))
-  colnames(gv) <- paste0("Trait.", 1:n_traits)
+  gv <- lapply(gxe_int, function(x) x + do.call(cbind, g_main))
+  gv <- do.call(rbind, mapply(function(x, y) cbind(x[rep(1:nrow(x), y), ]), x = gv, y = as.list(n_reps), SIMPLIFY = F))
+  colnames(gv) <- paste0("gv.Trait.", 1:n_traits)
 
   compsym_asr <- data.frame(
     env = envs,
     rep = reps,
     id = ids,
-    gv = gv
+    gv
   )
   compsym_asr <- compsym_asr[order(compsym_asr$env, compsym_asr$rep, compsym_asr$id), ]
 
   if (return_effects) {
     g_main <- mapply(cbind, list(data.frame(id = pop@id)), g_main = g_main, SIMPLIFY = F)
 
-    gxe_env <- pop@gv[, -seq(1, (n_traits + n_traits * n_envs), n_envs + 1)]
+    gxe_int <- pop@gv[, -seq(1, (n_traits + n_traits * n_envs), n_envs + 1)]
     index_eff <- as.list(as.data.frame(t(matrix(1:(n_traits * n_envs), nrow = n_traits, byrow = TRUE))))
-    gxe_env <- lapply(index_eff, function(x) gxe_env[, x])
-
-    eff_comps <- mapply(cbind, g_main, gxe_env = gxe_env, SIMPLIFY = F)
+    gxe_int <- lapply(index_eff, function(x) gxe_int[, x])
+    gxe_int <- lapply(gxe_int, function(x){colnames(x)=paste0("Env.", 1:n_envs);x})
+                      
+    eff_comps <- mapply(cbind, g_main, gxe_int = gxe_int, SIMPLIFY = F)
     eff_comps <- lapply(eff_comps, function(x) {
       x[[1]] <- factor(as.numeric(as.character(x[[1]])))
       x <- x[order(x[[1]]), ]
